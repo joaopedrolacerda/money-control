19b631fa673f6b2702f1e13fef4b6f2589fc175e
joao pedro lacerda <joaopedro.lacerda33@gmail.com>
2021-09-05 11:28:47 -0300

    feat: add green card on summary

--- src/components/summary/styles.ts
+++ src/components/summary/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: grid;
-
   grid-template-columns: repeat(3, 1fr);
   gap: 2rem;
   margin-top: -10rem;
@@ -11,7 +10,7 @@ export const Container = styled.div`
     background: var(--shape);
     padding: 1.5rem 2rem;
     border-radius: 0.25rem;
-    color: var(-text-title);
+    color: var(--text-title);
 
     header {
       display: flex;
@@ -25,6 +24,10 @@ export const Container = styled.div`
       font-size: 2rem;
       font-weight: 500;
       line-height: 3rem;
+    }
+    &.highlight-background {
+      background-color: var(--green);
+      color: #fff;
     }
   }
 `;
